bd7300c57e9bb716592f59f7445b61ef93cc0f3d
pabloApoca <pablo_mtv08@hotmail.com>
2023-07-07 20:37:15 -0300

    Primer commit con la base del proyecto.

---
+++ src/main/java/com/registro/UsuariosSpringSecurity/UsuariosSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.registro.UsuariosSpringSecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsuariosSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsuariosSpringSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/registro/UsuariosSpringSecurity/UsuariosSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.registro.UsuariosSpringSecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsuariosSpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
